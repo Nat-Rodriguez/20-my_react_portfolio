68627fa6df9ddcb6388dd953b26cd8163bc5b185
Nat <n3rodriguez2001@gmail.com>
2024-01-14 23:24:55 -0500

    attempting to add navbar

--- my_react_portfolio/src/App.jsx
+++ my_react_portfolio/src/App.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/header';
-import AboutMe from './components/About/about';
-import ProjectDisplay from './components/Project/projects';
-import Contacts from './components/Contact/contactpage';
-
+import Navbar from './components/Nav/navbar';
+import { useLocation } from 'react-router-dom';
+import Portfolio from './components/Portfolio/portfolio';
 
 function App() {
+  const currentPage = useLocation().pathname;
+
   return (
-    <div className="mainPage">
-      <Header />
-      <AboutMe />
-      {/* <ProjectDisplay /> */}
-      <Contacts />
-    </div>
+    <Router>
+      <div>
+        <Header>
+          <Navbar currentPage={currentPage} />
+        </Header>
+        <main>
+          <Portfolio/>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
